66861a41189b9aec09d816ea0380740a5b85c575
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-03-22 11:13:16 -0400

    updated push.sh

--- push.sh
+++ push.sh
@@ -3,8 +3,10 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+git branch
+
 touch temp
 git add .
 git commit -m "Updated build files"
 
-git push https://vutsalsinghal:${GH_TOKEN}@${TRAVIS_REPO_SLUG} HEAD:master > /dev/null 2>&1
+git push https://vutsalsinghal:${GH_TOKEN}@github.com/$vutsalsinghal/vutsalsinghal.github.io HEAD:master > /dev/null 2>&1
