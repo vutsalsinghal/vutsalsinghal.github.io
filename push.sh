62a87d72abbbf5e82ebd87c59b53b78fe791194b
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-03-22 10:51:59 -0400

    added gitignore

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-git config --global user.email "travis@travis.org"
-git config --global user.name "travis"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 git checkout master
 
 touch temp
 git add .
 git commit -m "Updated build files"
 
-git push https://vutsalsinghal:${GH_TOKEN}@TRAVIS_REPO_SLUG HEAD:master > /dev/null 2>&1
+git push https://vutsalsinghal:${GH_TOKEN}@${TRAVIS_REPO_SLUG} HEAD:master > /dev/null 2>&1
