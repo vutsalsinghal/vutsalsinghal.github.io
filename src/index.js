99de0a94bf877c19470d05908f390244208e9612
Vutsal Singhal <vutsalsinghal@gmail.com>
2019-03-24 15:24:23 -0400

    added google analytics

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import './libs/fontawesome/fontawesome-all.min';
 import './styles/base.scss';
 import App from './App';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-136789989-1');
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById('root'));
